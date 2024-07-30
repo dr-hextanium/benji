8ef1b32bb91720f0b03f2c82cb63c902a63ebad5
dr-hextanium <hextanium.on.discord@gmail.com>
2024-07-30 15:03:51 -0400

    add: base interfaces and documentation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/swerve/SwerveDrive.kt
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.hardware.drive.swerve
+
+import org.firstinspires.ftc.teamcode.hardware.drive.IDrive
+import org.firstinspires.ftc.teamcode.hardware.subsystems.ISubsystem
+import org.firstinspires.ftc.teamcode.utility.geometry.Pose2D
+
+class SwerveDrive : IDrive, ISubsystem {
+    override fun reset() {
+        TODO("Not yet implemented")
+    }
+
+    override fun read() {
+        TODO("Not yet implemented")
+    }
+
+    override fun move(pose: Pose2D) {
+        TODO("Not yet implemented")
+    }
+
+    override fun update() {
+        TODO("Not yet implemented")
+    }
+
+    override fun write() {
+        TODO("Not yet implemented")
+    }
+}
