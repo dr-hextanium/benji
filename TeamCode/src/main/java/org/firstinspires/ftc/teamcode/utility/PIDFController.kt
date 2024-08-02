0a49f3c151d009448c99baf52427421d31d896ea
dr-hextanium <hextanium.on.discord@gmail.com>
2024-08-02 17:20:47 -0400

    add: chainable methods to `PIDFController`

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/PIDFController.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/PIDFController.kt
@@ -12,20 +12,29 @@ class PIDFController(
     private var lastError = 0.0
     private var integral = 0.0
 
-    fun coeffs(p: Double = kP, i: Double = kI, d: Double = kD, f: Double = kF) {
+    fun coeffs(p: Double = kP, i: Double = kI, d: Double = kD, f: Double = kF): PIDFController {
         kP = p
         kI = i
         kD = d
         kF = f
+
+        return this
     }
 
-    fun reset() {
+    fun reset(): PIDFController {
         lastError = 0.0
         integral = 0.0
         before = System.nanoTime()
+
+        return this
     }
 
-    fun target(setpoint: Double) { this.target = setpoint; reset() }
+    fun target(setpoint: Double): PIDFController {
+        this.target = setpoint;
+        reset()
+
+        return this
+    }
 
     fun calculate(measured: Double): Double {
         val now = System.nanoTime()
