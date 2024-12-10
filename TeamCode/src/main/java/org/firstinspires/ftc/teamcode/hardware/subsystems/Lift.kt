d15166f1a672cc0499ce230cc3c39a95525a77d7
schsdrobotics <>
2024-12-10 17:26:13 -0500

    wowzas 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Lift.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/subsystems/Lift.kt
@@ -1,39 +1,55 @@
 package org.firstinspires.ftc.teamcode.hardware.subsystems
 
+import com.arcrobotics.ftclib.controller.PIDController
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import org.firstinspires.ftc.teamcode.opmode.debug.ExtendoTuner
+import org.firstinspires.ftc.teamcode.opmode.debug.PIDFTuner
+import kotlin.math.sign
 
-class Lift(val pinkLift: DcMotorEx, val blackLift: DcMotorEx) : ISubsystem {
-	var pinkPosition = 0
-	var blackPosition = 0
-	var targetPosition = 0
+class Lift(val pinkLift: DcMotorEx, val blackLift: DcMotorEx, val encoder: DcMotorEx) : ISubsystem {
+	var target = 0
+
+	val position: Int
+		get() = asInches(encoder.currentPosition).toInt()
+
+	private val controller = PIDController(kP, kI, kD)
+
+	private fun asInches(ticks: Int) = ticks.toDouble() / ticksPerInch
 
 	override fun reset() {
-		pinkLift.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
-		blackLift.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+		encoder.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+		blackLift.direction = DcMotorSimple.Direction.REVERSE
+		encoder.direction = DcMotorSimple.Direction.REVERSE
 
 		pinkLift.power = 0.0
 		blackLift.power = 0.0
-
-		pinkLift.targetPosition = 0
-		blackLift.targetPosition = 0
 	}
 
-	override fun read() {
-		pinkPosition = pinkLift.currentPosition
-		blackPosition = blackLift.currentPosition
-	}
+	override fun read() {  }
 
-	override fun update() {
-		pinkLift.targetPosition = targetPosition
-		blackLift.targetPosition = targetPosition
-	}
+	override fun update() {  }
 
 	override fun write() {
-		pinkLift.mode = DcMotor.RunMode.RUN_TO_POSITION
-		blackLift.mode = DcMotor.RunMode.RUN_TO_POSITION
+		val pid = controller.calculate(position.toDouble(), target.toDouble())
 
-		pinkLift.power = 1.0
-		blackLift.power = 1.0
+		val power = pid + (kS * sign(pid))
+
+		pinkLift.power = power
+		blackLift.power = power
+	}
+
+	companion object {
+		const val kP = 0.4
+		const val kI = 0.0
+		const val kD = 0.0
+		const val kS = 0.09
+
+		const val ticksPerInch: Int = 4670
+
+		const val HIGH_BASKET = 32
+		const val HIGH_BAR = 20
+		const val ZERO = 0
 	}
 }
